4bb9658ee6c51e44d4d5f7bf82b13b027318c28a
Shuffled720 <shuffled720@gmail.com>
2024-09-01 18:29:36 +0530

    till linked list done

--- src/main/java/com/shuffled/WorkingWithLinkedList.java
+++ src/main/java/com/shuffled/WorkingWithLinkedList.java
@@ -1,6 +1,7 @@
 package com.shuffled;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class WorkingWithLinkedList {
     public static void main(String[] args) {
@@ -9,6 +10,16 @@ public class WorkingWithLinkedList {
         linkedList.add(new Person("Alex1", 19));
         linkedList.add(new Person("Alex2", 20));
         linkedList.add(new Person("Alex3", 21));
+        linkedList.addFirst(new Person("Alex4",22));
+
+        ListIterator<Person> personListIterator=linkedList.listIterator();
+        while(personListIterator.hasNext()){
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+        while(personListIterator.hasPrevious()){
+            System.out.println(personListIterator.previous());
+        }
 
     }
     public static record Person(String name, int age){}
